1acefe7f4a70d34d963c81b0f8f728f021299934
kakgonbri <buikhacbin@gmail.com>
2025-01-19 00:27:02 +0700

    vnpay test

---
+++ src/main/java/vnpay/VNPayRedirectServlet.java
@@ -0,0 +1,79 @@
+
+package vnpay;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ASUS
+ */
+public class VNPayRedirectServlet extends HttpServlet {
+    private static final String JSP_PATH = "/WEB-INF/jsp/vnpay/";
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String path = request.getPathInfo(); // Extract the path from the URL
+        if (path == null || path.equals("/")) {
+            path = "options.jsp"; // Default to home.jsp
+        } else {
+            path = path.substring(path.lastIndexOf('/', path.length() - 1) + 1) + ".jsp";
+        }
+
+        String jspFilePath = JSP_PATH + path;
+        System.out.println(jspFilePath);
+        request.getRequestDispatcher(jspFilePath).forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
