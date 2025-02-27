f285a6e164fd951cad9662d4b857e09d52bfdddb
Quoc Nguyen <quocnguyen112505@gmail.com>
2025-02-27 13:27:14 +0700

    ok

--- src/main/java/filters/MaintenanceFilter.java
+++ src/main/java/filters/MaintenanceFilter.java
@@ -10,37 +10,39 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 /**
- * All requests have to go through the maintenance filter, this filter however, allows logging in, but only allows the admin to be logged in.
- * The maintenance filter is not fit for cases where tblUser is unaccessible, as this will prevent the admin from accessing the admin panel.
+ * All requests have to go through the maintenance filter, this filter however,
+ * allows logging in, but only allows the admin to be logged in.
+ * The maintenance filter is not fit for cases where tblUser is unaccessible, as
+ * this will prevent the admin from accessing the admin panel.
  */
 public class MaintenanceFilter implements jakarta.servlet.Filter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        if (service.ServerMaintenance.isActive()) {
-            HttpServletRequest httpRequest = (HttpServletRequest) request;
-            String path = httpRequest.getRequestURI().substring(httpRequest.getContextPath().length());
-
-            for (final String nmPath : config.Config.nonMaintenance) {
-                if (path.startsWith(nmPath)) {
-                    chain.doFilter(httpRequest, response);
-                    return;
-                }
-            }
-
-            HttpSession session = httpRequest.getSession(false);
-
-            if (session != null && ((model.User) session.getAttribute("user")).isAdmin()) {
-                chain.doFilter(httpRequest, response);
-                return;
-            }
-
-            ((HttpServletResponse) response).sendRedirect(httpRequest.getContextPath() + "/redirect?page=" + controller.RedirectServlet.REDIRECT_MAINTENANCE);
-
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        if (!service.ServerMaintenance.isActive()) {
+            chain.doFilter(request, response);
             return;
         }
 
-        chain.doFilter(request, response);
-    } // public void doFilter
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String path = httpRequest.getRequestURI().substring(httpRequest.getContextPath().length());
 
+        for (final String nmPath : config.Config.nonMaintenance) {
+            if (path.startsWith(nmPath)) {
+                chain.doFilter(httpRequest, response);
+                return;
+            }
+        }
+
+        HttpSession session = httpRequest.getSession(false);
+
+        if (session != null && ((model.User) session.getAttribute("user")).isAdmin()) {
+            chain.doFilter(httpRequest, response);
+            return;
+        }
+
+        ((HttpServletResponse) response).sendRedirect(
+                httpRequest.getContextPath() + "/redirect?page=" + controller.RedirectServlet.REDIRECT_MAINTENANCE);
+    } // public void doFilter
 }
