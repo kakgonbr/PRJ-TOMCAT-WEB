e5c1d8c6d44eaefe9feef0f933d6fde31603c00a
kakgonbri <buikhacbin@gmail.com>
2025-03-01 22:36:56 +0700

    gson map test

--- src/main/java/controller/websocket/ChatEndPoint.java
+++ src/main/java/controller/websocket/ChatEndPoint.java
@@ -4,6 +4,10 @@ import jakarta.servlet.http.HttpSession;
 import jakarta.websocket.*;
 import jakarta.websocket.server.ServerEndpoint;
 
+import java.io.IOException;
+import java.util.Map;
+import com.google.gson.Gson;
+
 @ServerEndpoint(value = "/chat", configurator = controller.websocket.ChatConfigurator.class)
 public class ChatEndPoint {
     private HttpSession httpSession;
@@ -31,6 +35,31 @@ public class ChatEndPoint {
     public void onMessage(String message, Session session) {
         service.Logging.logger.info("Message received: {}, from websocket session {}", message, session.getId());
         
+        java.lang.reflect.Type type = new com.google.gson.reflect.TypeToken<Map<String, String>>(){}.getType();
+        Map<String, String> map = (new Gson()).fromJson(message, type);
+        
+        if (targetId == -1) {
+            int target =Integer.parseInt(map.get("targetUser"));
+
+            try {
+                dao.ChatDAO.BoxManager.getBox(service.DatabaseConnection.getConnection(), ((model.User) httpSession.getAttribute("user")).getId(), target);
+                
+                targetId = target;
+            } catch (java.sql.SQLException e) {
+                service.Logging.logger.warn("Message from {} cannot be sent to user ID {}, reason: {} ,closing connection.", session.getId(), target, e.getMessage());
+                
+                try {
+                    session.close(new CloseReason(CloseReason.CloseCodes.VIOLATED_POLICY, "Unauthorized"));
+                } catch (IOException e1) {
+                    service.Logging.logger.warn("Failed to close the connection to {}, reason: {}", session.getId(), e1.getMessage());
+                }
+                return;
+            }
+
+            return;
+        } // if (targetId == -1)
+        
+        service.Logging.logger.info("Message to be delivered: {}, from websocket session {}", message, session.getId());
     }
 
     @OnClose
