725a258c93ed092e2130099c291ea48e2a708d0d
kakgonbri <buikhacbin@gmail.com>
2025-03-01 15:42:33 +0700

    removed locks

--- src/main/java/dao/ChatDAO.java
+++ src/main/java/dao/ChatDAO.java
@@ -7,31 +7,26 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class ChatDAO {
-    private static final String LOCK_TABLE_CHATBOX_BY_ID = "SELECT * FROM tblChatBox WHERE id = ? FOR UPDATE";
-    private static final String LOCK_TABLE_CHATBOX = "SELECT * FROM tblChatBox FOR UPDATE";
-    private static final String LOCK_TABLE_CHATCONTENT_BY_ID = "SELECT * FROM tblChatBoxContent WHERE id = ? FOR UPDATE";
-    private static final String LOCK_TABLE_CHATCONTENT = "SELECT * FROM tblChatBoxContent FOR UPDATE";
-
     public static class BoxManager {
         private static final String CREATE_BOX = "INSERT INTO tblChatBox (id, user1, user2)\n" + //
-        "VALUES (\n" + //
-        "    (SELECT MIN(t1.id) + 1 FROM tblChatBox t1 LEFT JOIN tblChatBox t2 \n" + //
-        "     ON t1.id + 1 = t2.id WHERE t2.id IS NULL),\n" + //
-        "    ?, ?\n" + //
-        ");";
+                "VALUES (\n" + //
+                "    (SELECT MIN(t1.id) + 1 FROM tblChatBox t1 LEFT JOIN tblChatBox t2 \n" + //
+                "     ON t1.id + 1 = t2.id WHERE t2.id IS NULL),\n" + //
+                "    ?, ?\n" + //
+                ");";
 
         public static synchronized void createBox(Connection connection, int user1, int user2) throws SQLException {
             try (Statement lockStatement = connection.createStatement();
-                ResultSet lockResultSet = lockStatement.executeQuery(LOCK_TABLE_CHATBOX)) {
-                
-                    try (PreparedStatement addPS = connection.prepareStatement(CREATE_BOX)) {
-                        addPS.setInt(1, user1);
-                        addPS.setInt(2, user2);
+                    ResultSet lockResultSet = lockStatement.executeQuery(LOCK_TABLE_CHATBOX)) {
 
-                        addPS.executeUpdate();
+                try (PreparedStatement addPS = connection.prepareStatement(CREATE_BOX)) {
+                    addPS.setInt(1, user1);
+                    addPS.setInt(2, user2);
 
-                        connection.commit();
-                    }
+                    addPS.executeUpdate();
+
+                    connection.commit();
+                }
             } catch (SQLException e) {
                 connection.rollback();
                 throw e;
@@ -39,69 +34,69 @@ public class ChatDAO {
         } // public static void createBox
 
         private static final String GET_BOXES_BY_USER = "SELECT * FROM tblChatBox WHERE (user1 = ?) OR (user2 = ?)";
-        public static synchronized java.util.List<model.ChatBox> getBoxes(Connection connection, int userId) throws SQLException {
-            try (Statement lockStatement = connection.createStatement();
-                ResultSet lockResultSet = lockStatement.executeQuery(LOCK_TABLE_CHATBOX)) {
-                
-                    try (PreparedStatement ps = connection.prepareStatement(GET_BOXES_BY_USER)) {
-                        ps.setInt(1, userId);
 
-                        ResultSet rs = ps.executeQuery();
+        public static synchronized java.util.List<model.ChatBox> getBoxes(Connection connection, int userId)
+                throws SQLException {
 
-                        java.util.List<model.ChatBox> contents = new java.util.ArrayList<>();
-                        while (rs.next()) {
-                            contents.add(model.ChatBox.fromResultSet(rs));
-                        }
+            try (PreparedStatement ps = connection.prepareStatement(GET_BOXES_BY_USER)) {
+                ps.setInt(1, userId);
 
-                        return contents;
-                    }
+                ResultSet rs = ps.executeQuery();
+
+                java.util.List<model.ChatBox> contents = new java.util.ArrayList<>();
+                while (rs.next()) {
+                    contents.add(model.ChatBox.fromResultSet(rs));
+                }
+
+                return contents;
             }
         } // public static java.util.List<model.ChatBox> getBoxes
 
         private static final String GET_BOX_BY_USERS = "SELECT * FROM tblChatBox WHERE (user1 = ? AND user2 = ?) OR (user1 = ? AND user2 = ?)";
-        public static synchronized model.ChatBox getBox(Connection connection, int user1, int user2) throws SQLException {
-            try (Statement lockStatement = connection.createStatement();
-                ResultSet lockResultSet = lockStatement.executeQuery(LOCK_TABLE_CHATBOX)) {
-                
-                    try (PreparedStatement ps = connection.prepareStatement(GET_BOX_BY_USERS)) {
-                        ps.setInt(1, user1);
-                        ps.setInt(2, user2);
-                        ps.setInt(3, user2);
-                        ps.setInt(4, user1);
-                        
-                        ResultSet rs = ps.executeQuery();
 
-                        if (rs.next()) {
-                            return model.ChatBox.fromResultSet(rs);
-                        }
+        public static synchronized model.ChatBox getBox(Connection connection, int user1, int user2)
+                throws SQLException {
 
-                        throw new java.sql.SQLException("CHATBOX DOES NOT EXIST");
-                    }
+            try (PreparedStatement ps = connection.prepareStatement(GET_BOX_BY_USERS)) {
+                ps.setInt(1, user1);
+                ps.setInt(2, user2);
+                ps.setInt(3, user2);
+                ps.setInt(4, user1);
+
+                ResultSet rs = ps.executeQuery();
+
+                if (rs.next()) {
+                    return model.ChatBox.fromResultSet(rs);
+                }
+
+                throw new java.sql.SQLException("CHATBOX DOES NOT EXIST");
             }
+
         } // public static model.ChatBox getBox
     } // public static class BoxManager
 
     public static class ContentManager {
-        private static final String CREATE_CONTENT = "INSERT INTO tblChatBoxContent (id, chatBoxID, message, time, senderID)\n" + //
-        "VALUES (\n" + //
-        "    (SELECT MIN(t1.id) + 1 FROM tblChatBoxContent t1 LEFT JOIN tblChatBoxContent t2 \n" + //
-        "     ON t1.id + 1 = t2.id WHERE t2.id IS NULL),\n" + //
-        "    ?, ?, ?, ?\n" + //
-        ");";
-        public static synchronized void createContent(Connection connection, int box, int sender, String message) throws SQLException {
-            try (Statement lockStatement = connection.createStatement();
-                ResultSet lockResultSet = lockStatement.executeQuery(LOCK_TABLE_CHATCONTENT)) {
-                
-                    try (PreparedStatement addPS = connection.prepareStatement(CREATE_CONTENT)) {
-                        addPS.setInt(1, box);
-                        addPS.setString(2, message);
-                        addPS.setDate(3, java.sql.Date.valueOf(java.time.LocalDate.now()));
-                        addPS.setInt(4, sender);
+        private static final String CREATE_CONTENT = "INSERT INTO tblChatBoxContent (id, chatBoxID, message, time, senderID)\n"
+                + //
+                "VALUES (\n" + //
+                "    (SELECT MIN(t1.id) + 1 FROM tblChatBoxContent t1 LEFT JOIN tblChatBoxContent t2 \n" + //
+                "     ON t1.id + 1 = t2.id WHERE t2.id IS NULL),\n" + //
+                "    ?, ?, ?, ?\n" + //
+                ");";
 
-                        addPS.executeUpdate();
+        public static synchronized void createContent(Connection connection, int box, int sender, String message)
+                throws SQLException {
 
-                        connection.commit();
-                    }
+            try (PreparedStatement addPS = connection.prepareStatement(CREATE_CONTENT)) {
+                addPS.setInt(1, box);
+                addPS.setString(2, message);
+                addPS.setDate(3, java.sql.Date.valueOf(java.time.LocalDate.now()));
+                addPS.setInt(4, sender);
+
+                addPS.executeUpdate();
+
+                connection.commit();
+
             } catch (SQLException e) {
                 connection.rollback();
                 throw e;
@@ -109,23 +104,23 @@ public class ChatDAO {
         } // public static void createContent
 
         private static final String GET_CONTENT_BY_BOX = "SELECT * FROM tblChatBoxContent WHERE chatBoxID = ?";
-        public static synchronized java.util.List<model.ChatContent> getContent(Connection connection, int box) throws SQLException {
-            try (Statement lockStatement = connection.createStatement();
-                ResultSet lockResultSet = lockStatement.executeQuery(LOCK_TABLE_CHATCONTENT)) {
-                
-                    try (PreparedStatement ps = connection.prepareStatement(GET_CONTENT_BY_BOX)) {
-                        ps.setInt(1, box);
 
-                        ResultSet rs = ps.executeQuery();
+        public static synchronized java.util.List<model.ChatContent> getContent(Connection connection, int box)
+                throws SQLException {
 
-                        java.util.List<model.ChatContent> contents = new java.util.ArrayList<>();
-                        while (rs.next()) {
-                            contents.add(model.ChatContent.fromResultSet(rs));
-                        }
+            try (PreparedStatement ps = connection.prepareStatement(GET_CONTENT_BY_BOX)) {
+                ps.setInt(1, box);
 
-                        return contents;
-                    }
+                ResultSet rs = ps.executeQuery();
+
+                java.util.List<model.ChatContent> contents = new java.util.ArrayList<>();
+                while (rs.next()) {
+                    contents.add(model.ChatContent.fromResultSet(rs));
+                }
+
+                return contents;
             }
+
         } // public static java.util.List<model.ChatContent> getContent
     } // public static class ContentManager
 }
--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -9,39 +9,33 @@ import java.sql.SQLException;
 import model.User;
 
 public final class UserDAO {
-    private static final String LOCK_TABLE_USER_BY_ID = "SELECT * FROM tblUser WHERE id = ? FOR UPDATE";
-    private static final String LOCK_TABLE_USER = "SELECT * FROM tblUser FOR UPDATE";
-
     public static final class UserManager {
-        private static final String CREATE_USER = "INSERT INTO tblUser (id, email, username, phoneNumber, password, persistentCookie, googleID, facebookID, isAdmin, credit, displayName, profileStringResourceID, bio)\n" + //
-        "VALUES (\n" + //
-        "    (SELECT MIN(t1.id) + 1 FROM tblUser t1 LEFT JOIN tblUser t2 \n" + //
-        "     ON t1.id + 1 = t2.id WHERE t2.id IS NULL),\n" + //
-        "    ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?\n" + //
-        ");";
+        private static final String CREATE_USER = "INSERT INTO tblUser (id, email, username, phoneNumber, password, persistentCookie, googleID, facebookID, isAdmin, credit, displayName, profileStringResourceID, bio)\n"
+                + //
+                "VALUES (\n" + //
+                "    (SELECT MIN(t1.id) + 1 FROM tblUser t1 LEFT JOIN tblUser t2 \n" + //
+                "     ON t1.id + 1 = t2.id WHERE t2.id IS NULL),\n" + //
+                "    ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?\n" + //
+                ");";
 
         public static synchronized void createUser(Connection connection, User user) throws SQLException {
-            try (Statement lockStatement = connection.createStatement();
-                ResultSet lockResultSet = lockStatement.executeQuery(LOCK_TABLE_USER)) {
-                
-                    try (PreparedStatement addPS = connection.prepareStatement(CREATE_USER)) {
-                        addPS.setString(1, user.getEmail());
-                        addPS.setString(2, user.getUsername());
-                        addPS.setString(3, user.getPhoneNumber());
-                        addPS.setString(4, user.getPassword());
-                        addPS.setString(5, user.getCookie());
-                        addPS.setString(6, user.getLinkStatus().getGoogleId());
-                        addPS.setString(7, user.getLinkStatus().getFacebookId());
-                        addPS.setBoolean(8, false);
-                        addPS.setLong(9, user.getCredit());
-                        addPS.setString(10, user.getDisplayName());
-                        addPS.setString(11, user.getProfilePicResource());
-                        addPS.setString(12, user.getBio());
+            try (PreparedStatement addPS = connection.prepareStatement(CREATE_USER)) {
+                addPS.setString(1, user.getEmail());
+                addPS.setString(2, user.getUsername());
+                addPS.setString(3, user.getPhoneNumber());
+                addPS.setString(4, user.getPassword());
+                addPS.setString(5, user.getCookie());
+                addPS.setString(6, user.getLinkStatus().getGoogleId());
+                addPS.setString(7, user.getLinkStatus().getFacebookId());
+                addPS.setBoolean(8, false);
+                addPS.setLong(9, user.getCredit());
+                addPS.setString(10, user.getDisplayName());
+                addPS.setString(11, user.getProfilePicResource());
+                addPS.setString(12, user.getBio());
 
-                        addPS.executeUpdate();
+                addPS.executeUpdate();
 
-                        connection.commit();
-                    }
+                connection.commit();
             } catch (SQLException e) {
                 connection.rollback();
                 throw e;
@@ -51,17 +45,13 @@ public final class UserDAO {
         private static final String DELETE_USER = "DELETE * FROM tblUser WHERE id = ?";
 
         public static synchronized void deleteUser(Connection connection, int id) throws SQLException {
-            try (PreparedStatement lockPS = connection.prepareStatement(LOCK_TABLE_USER_BY_ID)) {
-                lockPS.setInt(1, id);
-                lockPS.executeQuery();    
-            
-                    try (PreparedStatement ps = connection.prepareStatement(DELETE_USER)) {
-                        ps.setInt(1, id);
 
-                        ps.executeUpdate();
+            try (PreparedStatement ps = connection.prepareStatement(DELETE_USER)) {
+                ps.setInt(1, id);
 
-                        connection.commit();
-                    }
+                ps.executeUpdate();
+
+                connection.commit();
             } catch (SQLException e) {
                 connection.rollback();
                 throw e;
@@ -75,31 +65,28 @@ public final class UserDAO {
          * Careful, retrieve user's information from the database first
          */
         public static synchronized void updateUser(Connection connection, User user) throws SQLException {
-            try (PreparedStatement lockPS = connection.prepareStatement(LOCK_TABLE_USER_BY_ID)) {
-                lockPS.setInt(1, user.getId());
-                lockPS.executeQuery();    
-                
-                try (PreparedStatement addPS = connection.prepareStatement(UPDATE_USER)) {
-                    // 13
-                    addPS.setString(1, user.getEmail());
-                    addPS.setString(2, user.getUsername());
-                    addPS.setString(3, user.getPhoneNumber());
-                    addPS.setString(4, user.getPassword());
-                    addPS.setString(5, user.getCookie());
-                    addPS.setString(6, user.getLinkStatus().getGoogleId());
-                    addPS.setString(7, user.getLinkStatus().getFacebookId());
-                    addPS.setBoolean(8, false);
-                    addPS.setLong(9, user.getCredit());
-                    addPS.setString(10, user.getDisplayName());
-                    addPS.setString(11, user.getProfilePicResource());
-                    addPS.setString(12, user.getBio());
 
-                    addPS.setInt(13, user.getId());
-                    
-                    addPS.executeUpdate();
+            try (PreparedStatement addPS = connection.prepareStatement(UPDATE_USER)) {
+                // 13
+                addPS.setString(1, user.getEmail());
+                addPS.setString(2, user.getUsername());
+                addPS.setString(3, user.getPhoneNumber());
+                addPS.setString(4, user.getPassword());
+                addPS.setString(5, user.getCookie());
+                addPS.setString(6, user.getLinkStatus().getGoogleId());
+                addPS.setString(7, user.getLinkStatus().getFacebookId());
+                addPS.setBoolean(8, false);
+                addPS.setLong(9, user.getCredit());
+                addPS.setString(10, user.getDisplayName());
+                addPS.setString(11, user.getProfilePicResource());
+                addPS.setString(12, user.getBio());
 
-                    connection.commit();
-                    }
+                addPS.setInt(13, user.getId());
+
+                addPS.executeUpdate();
+
+                connection.commit();
+
             } catch (SQLException e) {
                 connection.rollback();
                 throw e;
@@ -112,21 +99,18 @@ public final class UserDAO {
          *
          * Careful, retrieve user's information from the database first
          */
-        public static synchronized void updateCookie(Connection connection, int userId, String cookie) throws SQLException {
-            try (PreparedStatement lockPS = connection.prepareStatement(LOCK_TABLE_USER_BY_ID)) {
-                lockPS.setInt(1, userId);
-                lockPS.executeQuery();    
-                
-                try (PreparedStatement addPS = connection.prepareStatement(UPDATE_COOKIE)) {
-                    // 2
-                    addPS.setString(1, cookie);
-                    addPS.setInt(2, userId);
+        public static synchronized void updateCookie(Connection connection, int userId, String cookie)
+                throws SQLException {
 
-                    
-                    addPS.executeUpdate();
+            try (PreparedStatement addPS = connection.prepareStatement(UPDATE_COOKIE)) {
+                // 2
+                addPS.setString(1, cookie);
+                addPS.setInt(2, userId);
 
-                    connection.commit();
-                    }
+                addPS.executeUpdate();
+
+                connection.commit();
+
             } catch (SQLException e) {
                 connection.rollback();
                 throw e;
@@ -134,57 +118,66 @@ public final class UserDAO {
         }
 
     } // public static final class UserManager
-    
+
     /**
      * 
      */
     public static final class UserFetcher {
         private static final String GET_USER_BY_ID = "SELECT * FROM tblUser WHERE id = ?";
-        
+
         public static synchronized model.User getUser(Connection connection, int id) throws SQLException {
             try (PreparedStatement ps = connection.prepareStatement(GET_USER_BY_ID)) {
                 ps.setInt(1, id);
 
                 try (ResultSet rs = ps.executeQuery()) {
-                    if (rs.next()) return model.User.fromResultSet(rs);
+                    if (rs.next())
+                        return model.User.fromResultSet(rs);
                 }
-            } 
+            }
             return null;
         } // public static model.User getUser
 
         private static final String GET_USER_BY_USERNAME = "SELECT * FROM tblUser WHERE username = ?";
-        public static synchronized model.User getUserFromUsername(Connection connection, String username) throws SQLException {
+
+        public static synchronized model.User getUserFromUsername(Connection connection, String username)
+                throws SQLException {
             try (PreparedStatement ps = connection.prepareStatement(GET_USER_BY_USERNAME)) {
                 ps.setString(1, username);
 
                 try (ResultSet rs = ps.executeQuery()) {
-                    if (rs.next()) return model.User.fromResultSet(rs);
+                    if (rs.next())
+                        return model.User.fromResultSet(rs);
                 }
-            } 
+            }
             throw new java.sql.SQLException("USER NOT FOUND");
         } // public static model.User getUser
-        
+
         private static final String GET_USER_BY_COOKIE = "SELECT * FROM tblUser WHERE persistentCookie = ?";
+
         public static synchronized model.User getUser(Connection connection, String cookie) throws SQLException {
             try (PreparedStatement ps = connection.prepareStatement(GET_USER_BY_COOKIE)) {
                 ps.setString(1, cookie);
 
                 try (ResultSet rs = ps.executeQuery()) {
-                    if (rs.next()) return model.User.fromResultSet(rs);
+                    if (rs.next())
+                        return model.User.fromResultSet(rs);
                 }
-            } 
+            }
             throw new java.sql.SQLException("USER NOT FOUND");
         } // public static model.User getUser
-        
+
         private static final String GET_USER_BY_USER = "SELECT * FROM tblUser WHERE username = ? AND password = ?";
         private static final String GET_USER_BY_EMAIL = "SELECT * FROM tblUser WHERE email = ? AND password = ?";
-        public static synchronized model.User getUser(Connection connection, String userOrEmail, String password) throws SQLException {
+
+        public static synchronized model.User getUser(Connection connection, String userOrEmail, String password)
+                throws SQLException {
             try (PreparedStatement ps = connection.prepareStatement(GET_USER_BY_USER)) {
                 ps.setString(1, userOrEmail);
                 ps.setString(2, password);
 
                 try (ResultSet rs = ps.executeQuery()) {
-                    if (rs.next()) return model.User.fromResultSet(rs);
+                    if (rs.next())
+                        return model.User.fromResultSet(rs);
                 }
             }
 
@@ -193,9 +186,10 @@ public final class UserDAO {
                 ps.setString(2, password);
 
                 try (ResultSet rs = ps.executeQuery()) {
-                    if (rs.next()) return model.User.fromResultSet(rs);
+                    if (rs.next())
+                        return model.User.fromResultSet(rs);
                 }
-            } 
+            }
 
             throw new java.sql.SQLException("USER NOT FOUND");
         } // public static synchronized model.User getUser
