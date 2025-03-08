36fe19a076f19b1ef7cb8b8b6394b9b18e1b2861
hoahtm <vunguyenkhoi47@gmail.com>
2025-03-08 20:21:48 +0700

    add createShop test

--- src/main/java/dao/ShopDAO.java
+++ src/main/java/dao/ShopDAO.java
@@ -4,10 +4,32 @@
  */
 package dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import java.sql.SQLException;
+import model.Shop;
+import service.DatabaseConnection;
+
 /**
  *
  * @author hoahtm
  */
 public class ShopDAO {
-    
+    public static final class ShopManager {
+        public static synchronized void createShop(Shop shop) throws SQLException {
+            try (EntityManager em = DatabaseConnection.getEntityManager()) {
+                EntityTransaction et = em.getTransaction();
+                try {
+                    et.begin();
+                    em.persist(shop);
+                    et.commit();
+                } catch (Exception e) {
+                    if (et.isActive()) {
+                        et.rollback();
+                    }
+                    throw new SQLException(e);
+                }
+            }
+        }
+    }
 }
