96f34800d28e695caaab0d981c9bba8018a08e1f
kakgonbri <buikhacbin@gmail.com>
2025-03-14 10:57:10 +0700

    removed parent field, hierarchy is maintained by JPA relationship

--- src/main/java/model/CategoryWrapper.java
+++ src/main/java/model/CategoryWrapper.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 public class CategoryWrapper {
     private int id;
     private String name;
-    private int parent;
+    // private int parent;
     private java.util.List<CategoryWrapper> children;
 
     public CategoryWrapper() {}
@@ -16,18 +16,18 @@ public class CategoryWrapper {
     public CategoryWrapper(Category category) {
         setId(category.getId());
         setName(category.getName());
-        setParent(category.getParentId().getId());
+        // setParent(category.getParentId() == null ? null : category.getParentId().getId());
         java.util.List<Category> categories = category.getCategoryList();
         children = categories.stream().map(CategoryWrapper::new).collect(Collectors.toList());
     }
 
-    public void setParent(int parent) {
-        this.parent = parent;
-    }
+    // public void setParent(int parent) {
+    //     this.parent = parent;
+    // }
 
-    public int getParent() {
-        return parent;
-    }
+    // public int getParent() {
+    //     return parent;
+    // }
 
     public java.util.List<CategoryWrapper> getChildren() {
         return children;
