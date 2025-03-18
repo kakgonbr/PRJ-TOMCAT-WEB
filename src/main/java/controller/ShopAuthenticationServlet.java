b80d89a0afd233f40d38331392f7bfba09237c64
Nguyen Thao <thtn.0507@gmail.com>
2025-03-19 00:43:19 +0700

    shopAuth

---
+++ src/main/java/controller/ShopAuthenticationServlet.java
@@ -0,0 +1,60 @@
+package controller;
+
+import dao.ShopDAO;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Shop;
+import model.User;
+
+public class ShopAuthenticationServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if (!sessionChecker(request, response)) {
+            return;
+        }
+
+        int shopId = getShop(request, response);
+        if (shopId != -1) {
+            response.sendRedirect("jsp/shopInfor.jsp");
+        } else {
+            response.sendRedirect("jsp/shopSignup.jsp");
+        }
+    }
+
+    private boolean sessionChecker(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+
+        if (user == null) {
+            response.sendRedirect("login.jsp");
+            return false;
+        }
+        return true;
+    }
+
+    private int getShop(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+
+        if (user == null) {
+            return -1;
+        }
+
+        try {
+            Shop shop = ShopDAO.ShopFetcher.getShopByOwnerId(user.getId());
+            return (shop != null) ? shop.getId() : -1;
+        } catch (SQLException ex) {
+            Logger.getLogger(ShopAuthenticationServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return -1; 
+    }
+}
