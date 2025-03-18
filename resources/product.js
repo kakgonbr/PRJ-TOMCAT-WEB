0d9d473339b085361b357e7104809c388e2a24ee
kakgonbri <buikhacbin@gmail.com>
2025-03-18 10:40:58 +0700

    javascript fix

--- resources/product.js
+++ resources/product.js
@@ -14,7 +14,7 @@ function fetchProducts(query, filter, shopId) {
     }
 
     if (filter) {
-        url.searchParams.append('category', filter.value)
+        url.searchParams.append('category', filter)
     }
 
     if (shopId) {
@@ -81,5 +81,6 @@ function fetchByCategory() { // store category in global
 }
 
 function fetchByShop() { // store shop in global
-    fetchProducts(null, document.querySelector('input[name="filter"]:checked'), shopId);
+    filter = document.querySelector('input[name="filter"]:checked');
+    fetchProducts(null, filter? filter.value : 0, shopId);
 }
