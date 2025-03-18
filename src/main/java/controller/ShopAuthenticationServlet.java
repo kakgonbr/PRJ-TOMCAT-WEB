27919524298b8c7731cf6ea90cb8eb897ffc788a
Nguyen Thao <thtn.0507@gmail.com>
2025-03-19 01:46:40 +0700

    f

--- src/main/java/controller/ShopAuthenticationServlet.java
+++ src/main/java/controller/ShopAuthenticationServlet.java
@@ -23,7 +23,7 @@ public class ShopAuthenticationServlet extends HttpServlet {
 
         int shopId = getShop(request, response);
         if (shopId != -1) {
-            response.sendRedirect(request.getContextPath() + "jsp/shopInfo.jsp");
+            response.sendRedirect(request.getContextPath() + "/jsp/shopInfo.jsp");
         } else {
             response.sendRedirect(request.getContextPath() + "/shop-signup");
         }
