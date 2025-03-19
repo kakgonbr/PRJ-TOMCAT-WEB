947bd51490b90cebb2c41629f24c6f3e6587f901
Nguyen Thao <thtn.0507@gmail.com>
2025-03-20 03:52:41 +0700

    fix

--- src/main/java/controller/ShopHomeServlet.java
+++ src/main/java/controller/ShopHomeServlet.java
@@ -1,33 +1,33 @@
 package controller;
- 
- import java.io.IOException;
- import java.io.PrintWriter;
- import jakarta.servlet.ServletException;
- import jakarta.servlet.http.HttpServlet;
- import jakarta.servlet.http.HttpServletRequest;
- import jakarta.servlet.http.HttpServletResponse;
- 
- /**
-  *
-  * @author hoahtm
-  */
- public class ShopHomeServlet extends HttpServlet {
- 
-     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-         String shopIdParam = request.getParameter("shopId");
- 
-         if (shopIdParam == null || shopIdParam.isEmpty()) {
-             response.sendRedirect(request.getContextPath() + "/shop-signup");
-             return;
-         }
- 
-         int shopId = Integer.parseInt(shopIdParam);
-         request.setAttribute("shopId", shopId);
-         request.getRequestDispatcher(config.Config.JSPMapper.SELLER_CENTER).forward(request, response); 
-     }
- 
-     @Override
-     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
- 
-     }
- }
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ *
+ * @author hoahtm
+ */
+public class ShopHomeServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String shopIdParam = request.getParameter("shopId");
+        HttpSession session = request.getSession();
+        Integer shopId = (Integer) session.getAttribute("shopId");
+        if (shopId == null) {
+            response.sendRedirect(request.getContextPath() + "/shop-signup");
+            return;
+        }
+
+        request.setAttribute("shopId", shopId);
+        request.getRequestDispatcher(config.Config.JSPMapper.SELLER_CENTER).forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
