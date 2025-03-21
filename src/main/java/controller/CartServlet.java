c933d1fe857588f98326c385044b227c370b0637
Vu Nguyen Khoi <vunguyenkhoi47@gmail.com>
2025-03-22 04:35:07 +0700

    fix cart

--- src/main/java/controller/CartServlet.java
+++ src/main/java/controller/CartServlet.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/cart")
 public class CartServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
