58547e89e7a74e29912c9c5614f781292e04f3f4
kakgonbri <buikhacbin@gmail.com>
2025-03-22 21:53:06 +0700

    database parity, correct id generation for Cart.java

--- src/main/java/model/Cart.java
+++ src/main/java/model/Cart.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -38,6 +40,7 @@ public class Cart implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @NotNull
     @Column(name = "id")
