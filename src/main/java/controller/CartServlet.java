e5c1aacc6e393ceac55517b6b4c07fa315151780
dat-21 <tiendatyyy2005@gmail.com>
2025-03-22 23:51:16 +0700

    Update CartServlet.java

--- src/main/java/controller/CartServlet.java
+++ src/main/java/controller/CartServlet.java
@@ -2,12 +2,11 @@ package controller;
 
 import java.io.IOException;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/cart")
+
 public class CartServlet extends HttpServlet {
 
     @Override
