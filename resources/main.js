15ad8a2368b34e0ac3f2f94c413f272e89abd469
Quoc Nguyen <quocnguyen112505@gmail.com>
2025-03-24 18:26:43 +0700

    fix main js

--- resources/main.js
+++ resources/main.js
@@ -44,36 +44,44 @@ function closeError() {
 document.addEventListener("DOMContentLoaded", startupTheme);
 
 //clear btn 
-const searchBar = document.querySelector('#searchBar');
-const clearButton = document.querySelector('.clear-btn');
+document.addEventListener("DOMContentLoaded", () => {
+    const searchBar = document.querySelector('#searchBar');
+    const clearButton = document.querySelector('.clear-btn');
 
-clearButton.addEventListener('click', () => {
-    searchBar.value = '';
-    clearButton.style.display = 'none';
-});   
-searchBar.addEventListener('input',() => {
-    clearButton.style.display = searchBar.value ? 'block':'none';
+    clearButton.addEventListener('click', () => {
+        searchBar.value = '';
+        clearButton.style.display = 'none';
+    });
+
+    searchBar.addEventListener('input', () => {
+        clearButton.style.display = searchBar.value ? 'block' : 'none';
+    });
 });
+
+
 //dropdown menu display
-const category = document.getElementById('category');
-const menu = document.querySelector('.dropdown-menu-hover');
-let timeoutId = null;
+document.addEventListener("DOMContentLoaded", () => {
+    const category = document.getElementById('category');
+    const menu = document.querySelector('.dropdown-menu-hover');
+    let timeoutId = null;
 
-function showMenu() {
-    clearTimeout(timeoutId); 
-    menu.style.display = 'block';
-}
+    function showMenu() {
+        clearTimeout(timeoutId); 
+        menu.style.display = 'block';
+    }
 
-function hideMenuAfterDelay() {
-    timeoutId = setTimeout(() => {
-        menu.style.display = 'none';
-    }, 300); 
-}
+    function hideMenuAfterDelay() {
+        timeoutId = setTimeout(() => {
+            menu.style.display = 'none';
+        }, 300); 
+    }
 
-category.addEventListener('mouseenter', showMenu);
-category.addEventListener('mouseleave', hideMenuAfterDelay);
-menu.addEventListener('mouseenter', showMenu);
-menu.addEventListener('mouseleave', hideMenuAfterDelay);
+    category.addEventListener('mouseenter', showMenu);
+    menu.addEventListener('mouseenter', showMenu);
+
+    category.addEventListener('mouseleave', hideMenuAfterDelay);
+    menu.addEventListener('mouseleave', hideMenuAfterDelay);
+});
 /*category */
 async function fetchCategoriesHeader() {
     try {
