bc0ebf46511df415fef2cef6e650313626618547
kakgonbri <buikhacbin@gmail.com>
2025-03-25 23:54:38 +0700

    adding variables to event instead of on javascript load

--- resources/searchPage.js
+++ resources/searchPage.js
@@ -1,58 +1,7 @@
-/*price range input*/
-let rangeInput = document.querySelectorAll('.range-input input');
-let rangeText = document.querySelectorAll('.range-text input');
-let progress = document.querySelector('.progress');
-let priceMax= rangeInput[0].max;
-const priceGap = 1000;
-        
-rangeInput.forEach(input => {
-    input.addEventListener('input', (e) => {
-        let minVal = parseInt(rangeInput[0].value);
-        let maxVal = parseInt(rangeInput[1].value);
-        if(maxVal - minVal < priceGap) {
-            if(e.target.className === 'range-min'){
-                rangeInput[0].value = maxVal - priceGap;
-            }else{
-                rangeInput[1].value = minVal + priceGap;
-            }
-        }
-        else {
-            let positionMin = (minVal / priceMax) * 100;
-            let positionMax  = 100 - ((maxVal / priceMax) * 100);
-            progress.style.left = positionMin + '%';
-            progress.style.right = positionMax + '%';
-            rangeText[0].value = minVal;
-            rangeText[1].value = maxVal;
-        }
-    })
-});
-
-rangeText.forEach( (input) => {
-    input.addEventListener('input', e =>{
-        let minVal = parseInt(rangeText[0].value);
-        let maxVal = parseInt(rangeText[1].value);
-        
-        if((maxVal - minVal >= priceGap) && maxVal <= rangeInput[1].max){
-            if(e.target.className == 'form-control input-min'){
-                rangeInput[0].value = minVal;
-                progress.style.left = ((minVal / priceMax) * 100) + "%";
-            }else{
-                rangeInput[1].value = maxVal;
-                progress.style.right = 100 - (maxVal / priceMax) * 100 + "%";
-            }
-        }
-    });
-});
-
-/*Sort by*/
-const dropdownItems = document.querySelectorAll(".dropdown-item");
-const chosenElement = document.querySelector(".sort-menu-chosen");
-
-dropdownItems.forEach(item => {
-        item.addEventListener("click", function (event) {
-        chosenElement.textContent = this.textContent.trim();
-    });
-});
+let rangeInput;
+let rangeText;
+let progress;
+let priceMax;
 
 /**/
 async function fetchCategories() {
@@ -203,7 +152,62 @@ async function fetchProductsSearch(byParam = false) {
     return li;
   }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {/*price range input*/
+    rangeInput = document.querySelectorAll('.range-input input');
+    rangeText = document.querySelectorAll('.range-text input');
+    progress = document.querySelector('.progress');
+    priceMax = rangeInput[0].max;
+    const priceGap = 1000;
+            
+    rangeInput.forEach(input => {
+        input.addEventListener('input', (e) => {
+            let minVal = parseInt(rangeInput[0].value);
+            let maxVal = parseInt(rangeInput[1].value);
+            if(maxVal - minVal < priceGap) {
+                if(e.target.className === 'range-min'){
+                    rangeInput[0].value = maxVal - priceGap;
+                }else{
+                    rangeInput[1].value = minVal + priceGap;
+                }
+            }
+            else {
+                let positionMin = (minVal / priceMax) * 100;
+                let positionMax  = 100 - ((maxVal / priceMax) * 100);
+                progress.style.left = positionMin + '%';
+                progress.style.right = positionMax + '%';
+                rangeText[0].value = minVal;
+                rangeText[1].value = maxVal;
+            }
+        })
+    });
+    
+    rangeText.forEach( (input) => {
+        input.addEventListener('input', e =>{
+            let minVal = parseInt(rangeText[0].value);
+            let maxVal = parseInt(rangeText[1].value);
+            
+            if((maxVal - minVal >= priceGap) && maxVal <= rangeInput[1].max){
+                if(e.target.className == 'form-control input-min'){
+                    rangeInput[0].value = minVal;
+                    progress.style.left = ((minVal / priceMax) * 100) + "%";
+                }else{
+                    rangeInput[1].value = maxVal;
+                    progress.style.right = 100 - (maxVal / priceMax) * 100 + "%";
+                }
+            }
+        });
+    });
+    
+    /*Sort by*/
+    const dropdownItems = document.querySelectorAll(".dropdown-item");
+    const chosenElement = document.querySelector(".sort-menu-chosen");
+    
+    dropdownItems.forEach(item => {
+            item.addEventListener("click", function (event) {
+            chosenElement.textContent = this.textContent.trim();
+        });
+    });
+    
     fetchCategories();
 });
 
