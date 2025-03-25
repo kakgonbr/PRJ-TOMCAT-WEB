f22a81b2852cef74b8a8c0d2865ed98e993e1481
kakgonbri <buikhacbin@gmail.com>
2025-03-25 22:02:30 +0700

    cascading delete operation from order to ordered items

--- src/main/java/model/ProductOrder.java
+++ src/main/java/model/ProductOrder.java
@@ -71,7 +71,7 @@ public class ProductOrder implements Serializable {
     private User userId;
     @Column(name = "status")
     private Boolean status;
-    @OneToMany(mappedBy = "orderId")
+    @OneToMany(mappedBy = "orderId", cascade = CascadeType.ALL)
     private List<OrderedItem> orderedItemList;
 
     public ProductOrder() {
