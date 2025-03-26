e4bc4c499b011805cf3b8375dd7a803428948f66
Quoc Nguyen <quocnguyen112505@gmail.com>
2025-03-26 18:04:43 +0700

    stupid js

--- src/main/java/controller/ajax/MapAPI.java
+++ src/main/java/controller/ajax/MapAPI.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import org.apache.http.client.ClientProtocolException;
+
 public class MapAPI extends HttpServlet {
 
     @Override
